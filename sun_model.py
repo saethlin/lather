7600623ca37e13599f033fa3167cc4feba2ec98d
Saethlin <kimockb@gmail.com>
2016-11-17 13:59:38 -0500

    prototype of new parsing

--- sun_model.py
+++ sun_model.py
@@ -1,21 +1,23 @@
 import numpy as np
 import lather
 
-spots = np.loadtxt('g2013.txt', usecols=[2, 10, 11, 12], unpack=True)
+spots = []
+with open('g2013.txt') as spots_file:
+    for line in spots_file:
+        day = float(line[6:8])
+        size = float(line[40:44])/1e6
+        longitude = float(line[57:62])
+        latitude = float(line[63:68])
 
-spots[:, -1] /= 1e6  # Correct millionths of a stellar hemisphere to hemispheres
+        spots.append((day, (latitude, longitude, size, False)))
 
 sim = lather.Simulation('config.cfg')
 sim.clear_spots()
 
+results = []
+
 for spot, next_spot in zip(spots, spots[1:]):
-    sim.add_spot(spot[4], spot[3], spot[2], False)
-
-    if next_spot[0] != spot[0]:
-        print(sim)
-        exit()
-        sim.observe(np.array([]))
-
-
-
-
+    sim.add_spot(*spot[1])
+    if spot[0] != next_spot[0]:
+        results.append(sim.observe(np.array([spot[0]])))
+        sim.clear_spots()
